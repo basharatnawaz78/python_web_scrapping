1f043bb96a1aeaec000491dc0caac2b1ca3ffa2f
Abhishek <205119002@nitt.edu>
2020-11-26 12:01:34 +0530

    added reviews count

--- flipkart/views.py
+++ flipkart/views.py
@@ -19,14 +19,26 @@ def submit(request):
     page = requests.get(url)
     soup = BeautifulSoup(page.content)
 
-    name = soup.find(class_='_4rR01T') #name
-    price = soup.find(class_='_1_WHN1') #price
+    name = None #name
+    if(soup.find(class_='_4rR01T')==None):
+        name = soup.find(class_='s1Q9rs')
+    else:
+        name = soup.find(class_='_4rR01T')
+    price = soup.find(class_='_30jeq3') #price
+    
     reviews = soup.find(class_='_3LWZlK') #review points
+    rev = soup.select('span._2_R_DZ')
+    rev2 = rev[0].find_all('span')
+    people_rated = rev2[1].text
+    people_reviewed = rev2[3].text
+
 
     context = {
         'name':name.text,
         'price':price.text,
         'reviews':reviews.text,
+        'people_rated':people_rated,
+        'people_reviewed':people_reviewed,
     }
     print(url)
     return render(request,'submit.html',context)
